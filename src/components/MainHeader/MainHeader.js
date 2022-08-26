99e8aa763bc143c423ab87b53c2d4cbabbfe2e89
AdilBulsari <adilbulsari@gmail.com>
2022-08-26 21:56:59 +0100

    reusable component

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import Navigation from "./Navigation";
 import classes from "./MainHeader.module.css";
+import AuthContext from "../store/auth-context";
 
 const MainHeader = (props) => {
+  const { setIsLoggedIn } = useContext(AuthContext);
+
+  const logoutHandler = () => {
+    localStorage.removeItem("userLogin");
+    setIsLoggedIn(false);
+    localStorage.clear();
+  };
+
   return (
     <header className={classes["main-header"]}>
       <h1>A Typical Page</h1>
-      <Navigation onLogout={props.onLogout} />
+      <Navigation onLogout={logoutHandler} />
     </header>
   );
 };
